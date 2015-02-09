417a838af1fde4e37379ac750e7569c85ec2d224
fffw <fffw@getlantern.org>
2015-02-09 20:52:47 +0800

    stop gcc from complain on linux

--- pacon_linux.c
+++ pacon_linux.c
@@ -4,7 +4,11 @@
 int togglePac(int onOff, const char* pacUrl)
 {
 	int ret = 0;
-	g_type_init(); // deprecated since version 2.36
+
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+	// deprecated since version 2.36, must leave here or prior glib will crash
+	g_type_init();
+#pragma GCC diagnostic warning "-Wdeprecated-declarations"
 	GSettings* setting = g_settings_new("org.gnome.system.proxy");
 	if (onOff == PAC_ON) {
 		gboolean success = g_settings_set_string(setting, "mode", "auto");
