f976af3affb723037b9fc6b7ffb7292f891335b2
Ox Cart <ox.to.a.cart@gmail.com>
2015-02-19 10:20:01 -0600

    Added linux 386 pac bytes

--- pac2go.sh
+++ pac2go.sh
@@ -20,6 +20,6 @@ osslsigncode sign -pkcs12 "$BNS_CERT" -pass "$BNS_CERT_PASS" -in $BINPATH/window
 codesign -s "Developer ID Application: Brave New Software Project, Inc" -f $BINPATH/darwin/pac || die "Could not sign macintosh"
 
 go-bindata -nomemcopy -nocompress -pkg pac -prefix $BINPATH/darwin -o pac_bytes_darwin.go $BINPATH/darwin
-#go-bindata -nomemcopy -nocompress -pkg pac -prefix binaries/linux_386 -o pac_bytes_linux_386.go $BINPATH/linux_386
+go-bindata -nomemcopy -nocompress -pkg pac -prefix $BINPATH/linux_386 -o pac_bytes_linux_386.go $BINPATH/linux_386
 go-bindata -nomemcopy -nocompress -pkg pac -prefix $BINPATH/linux_amd64 -o pac_bytes_linux_amd64.go $BINPATH/linux_amd64
 go-bindata -nomemcopy -nocompress -pkg pac -prefix $BINPATH/windows -o pac_bytes_windows.go $BINPATH/windows    
--- pac_nondarwin.go
+++ pac_nondarwin.go
@@ -2,6 +2,10 @@
 
 package pac
 
+import (
+	"github.com/getlantern/byteexec"
+)
+
 func ensureElevatedOnDarwin(be *byteexec.Exec, helperFullPath string, prompt string, iconFullPath string) (err error) {
 	return nil
 }
